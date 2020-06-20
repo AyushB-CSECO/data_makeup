e0651c883fbd7f14dd01da7353d1cd64907cfeff
Ayush Bansal <ayushb3131@gmail.com>
2020-06-20 10:23:53 +0530

    Code process 2

--- data_visualisation.py
+++ data_visualisation.py
@@ -1,6 +1,7 @@
-import mysql.connector as sqlcon
+import mysql.connector as sqlcon  # Calling required libraries and functions
 import pandas as pd
 
+# Establishing Connection and calling data from MySQL database
 conn = sqlcon.connect(user = 'root', password = 'aybs1196', host = '127.0.0.1', database = 'py_vis')
 query = 'SELECT * FROM plcmnt_stats'
 cursor = conn.cursor()
@@ -9,5 +10,19 @@ plcmnt_records = cursor.fetchall()
 plcmnt_records = pd.DataFrame(plcmnt_records)
 colnames = list([i[0] for i in cursor.description])
 plcmnt_records.columns = colnames
-print(plcmnt_records)
+# print(plcmnt_records)
 cursor.close()
+
+
+# Replacing char values in columns with binaries for efficient working
+data = plcmnt_records.copy()
+data['sex'] = data['sex'].replace(['F','M'], [0,1])
+data['ssc_b'] = data['ssc_b'].replace(['Others', 'Central'], [0,1])
+data['hsc_b'] = data['hsc_b'].replace(['Others', 'Central'], [0,1])
+data['hsc_s'] = data['hsc_s'].replace(['Commerce', 'Science', 'Arts'], [0,1,2])
+data['degree_t'] = data['degree_t'].replace(['Sci&Tech', 'Comm&Mgmt', 'Others'], [0,1,2])
+data['workex'] = data['workex'].replace(['No', 'Yes'], [0,1])
+data['specialisation'] = data['specialisation'].replace(['Mkt&HR','Mkt&Fin'], [0,1])
+data['status'] = data['status'].replace(['Placed', 'Not Placed'], [0,1])
+
+print(data)
